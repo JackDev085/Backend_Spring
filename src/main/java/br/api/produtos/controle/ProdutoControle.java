f06232e84ce61954b8a4e0b916484ff848d696c6
Jackson Lorran <computadordb@gmail.com>
2023-08-28 13:40:29 -0300

    Adicionando os métódos cadastrar / alterar

--- src/main/java/br/api/produtos/controle/ProdutoControle.java
+++ src/main/java/br/api/produtos/controle/ProdutoControle.java
@@ -1,7 +1,11 @@
 package br.api.produtos.controle;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.api.produtos.modelo.ProdutoModelo;
@@ -10,6 +14,14 @@ import br.api.produtos.serviço.ProdutoServico;
 @RestController
 public class ProdutoControle {
 
+    @PostMapping("/cadastrar")
+    public ResponseEntity<?> cadastrar(@RequestBody ProdutoModelo pm){
+        return ps.cadastrarAlterar(pm, "cadastrar");
+    }
+    @PutMapping("/alterar")
+    public ResponseEntity<?> alterar(@RequestBody ProdutoModelo pm){
+        return ps.cadastrarAlterar(pm, "alterar");
+    }
     @Autowired
     private ProdutoServico ps;
     @GetMapping("/listar")
--- src/main/java/br/api/produtos/serviço/ProdutoServico.java
+++ src/main/java/br/api/produtos/serviço/ProdutoServico.java
@@ -1,9 +1,12 @@
 package br.api.produtos.serviço;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import br.api.produtos.modelo.ProdutoModelo;
+import br.api.produtos.modelo.RespostaModelo;
 import br.api.produtos.repositorio.ProdutoRepositorio;
 
 @Service
@@ -13,5 +16,24 @@ public class ProdutoServico {
     //Método para listar todos os produtos
     public Iterable<ProdutoModelo>listar(){
         return pr.findAll();
+    }
+    @Autowired
+    private RespostaModelo rm;
+    //método para cadastrar ou alterar produtos
+    public ResponseEntity<?>cadastrarAlterar(ProdutoModelo pm, String acao){
+        if(pm.getNome().equals("")){
+            rm.setMensagem("O nome do produto é obrigatório");
+            return new ResponseEntity<RespostaModelo>(rm,HttpStatus.BAD_REQUEST);
+        }else if(pm.getMarca().equals("")){
+            rm.setMensagem( " O nome da marca é obrigatório");
+            return new ResponseEntity<RespostaModelo>(rm,HttpStatus.BAD_REQUEST);
+        }else{
+            if(acao.equals("cadastrar")){
+                return new ResponseEntity<ProdutoModelo>(pr.save(pm),HttpStatus.CREATED);
+            }
+            else{
+                return new ResponseEntity<ProdutoModelo>(pr.save(pm),HttpStatus.OK);
+            }
+        }
     }
 }
